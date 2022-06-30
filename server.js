ffea6dfed8391634c973d9a1ede4d76417ad677e
Samuel Raducan <samuel.thembp@gmail.com>
2022-06-30 17:57:48 +0100

    Hashed the password and add logic to save the file into the uploads folder and db

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ require('dotenv').config();
 const multer = require('multer');
 const mongoose = require('mongoose');
 const path = require('path');
+const File = require('./models/File');
+const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
-const port = 3000;
 
 const upload = multer({ dest: 'uploads' });
 
@@ -21,8 +22,19 @@ app.get('/', (req, res) => {
 	res.render('index');
 });
 
-app.post('/upload', upload.single('file'), (req, res) => {
-	res.send('HI.');
+app.post('/upload', upload.single('file'), async (req, res) => {
+	const fileData = {
+		path: req.file.path, // Multer
+		originalName: req.file.originalname,
+	};
+
+	if (req.body.password !== null && req.body.password !== '') {
+		fileData.password = await bcrypt.hash(req.body.password, 10);
+	}
+
+	const file = await File.create(fileData);
+	console.log(file);
+	res.send(file.originalName);
 });
 
 app.listen(process.env.PORT);
