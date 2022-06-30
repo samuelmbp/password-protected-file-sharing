d7e8eb6948fcc7d1c05dcb5179a49bb04a03fd77
Samuel Raducan <samuel.thembp@gmail.com>
2022-06-30 13:45:51 +0100

    Mongoose DB setup

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+require('dotenv').config();
 const multer = require('multer');
+const mongoose = require('mongoose');
 const path = require('path');
 const express = require('express');
 const app = express();
 const port = 3000;
 
 const upload = multer({ dest: 'uploads' });
+
+// DB Connection
+mongoose.connect(process.env.DATABASE_URL);
 
 // Template Engine
 app.set('view engine', 'ejs');
@@ -20,4 +25,4 @@ app.post('/upload', upload.single('file'), (req, res) => {
 	res.send('HI.');
 });
 
-app.listen(port, () => console.log(`Listening on port: ${port}`));
+app.listen(process.env.PORT);
