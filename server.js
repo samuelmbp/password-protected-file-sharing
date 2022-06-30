b251c5f8aee8a61cd58dee444c04d47804970afc
Samuel Raducan <samuel.thembp@gmail.com>
2022-06-30 18:29:46 +0100

    Add logic to download the file

--- server.js
+++ server.js
@@ -33,8 +33,18 @@ app.post('/upload', upload.single('file'), async (req, res) => {
 	}
 
 	const file = await File.create(fileData);
-	console.log(file);
-	res.send(file.originalName);
+	res.render('index', { fileLink: `${req.headers.origin}/file/${file.id}` }); // localhost
+});
+
+app.get('/file/:id', async (req, res) => {
+	const file = await File.findById(req.params.id);
+
+	file.downloadCount++;
+	file.save();
+	console.log(file.downloadCount);
+
+	// Download the file for the user
+	res.download(file.path, file.originalName);
 });
 
 app.listen(process.env.PORT);
