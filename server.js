f58403d894d592463947508e809017b3fb2b175f
Samuel Raducan <samuel.thembp@gmail.com>
2022-06-30 13:08:12 +0100

    Create the form and use multer to upload files

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
+const multer = require('multer');
+const path = require('path');
 const express = require('express');
 const app = express();
+const port = 3000;
 
+const upload = multer({ dest: 'uploads' });
+
+// Template Engine
 app.set('view engine', 'ejs');
+
+// Static Files
+// app.use('/css', express.static(path.join(__dirname + 'public/css')));
+
 app.get('/', (req, res) => {
 	res.render('index');
 });
 
-app.listen(3000);
+app.post('/upload', upload.single('file'), (req, res) => {
+	res.send('HI.');
+});
+
+app.listen(port, () => console.log(`Listening on port: ${port}`));
